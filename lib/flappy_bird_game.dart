280a383ec4df31488b26d5a3b222fd4cb73186e0
Yulu Xu <xuyulu86@126.com>
2022-10-16 23:36:05 +0800

    add bouncing

--- lib/flappy_bird_game.dart
+++ lib/flappy_bird_game.dart
@@ -8,11 +8,13 @@ class FlappyBirdGame extends StatefulWidget {
 }
 
 class _FlappyBirdGameState extends State<FlappyBirdGame> {
+  final double _step = 0.5;
   double _birdY = 0.0;
   final String initBirdImage = 'assets/images/bird0_1.png';
   final String upBirdImage = 'assets/images/bird0_0.png';
   final String downBirdImage = 'assets/images/bird0_2.png';
   late String _birdImage = initBirdImage;
+  late Curve curve = Curves.bounceInOut;
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +23,11 @@ class _FlappyBirdGameState extends State<FlappyBirdGame> {
       child: AnimatedContainer(
         width: double.infinity,
         height: double.infinity,
-        alignment: Alignment(-0.8, _birdY),
+        color: Colors.green[300],
+        alignment: Alignment(-0.7, _birdY),
         duration: const Duration(milliseconds: 300),
         onEnd: _moveDown,
-        curve: Curves.easeInOut,
+        curve: curve,
         child: Image.asset(_birdImage),
       ),
     );
@@ -32,17 +35,22 @@ class _FlappyBirdGameState extends State<FlappyBirdGame> {
 
   void _moveUp() {
     setState(() {
-      _birdY -= 0.5;
+      if (_birdY - _step >= -1) {
+        _birdY -= _step;
+      }
       _birdImage = upBirdImage;
+      curve = Curves.bounceInOut;
     });
   }
 
   void _moveDown() {
     setState(() {
-      if (_birdY <= 0.5) {
-        _birdY += 0.5;
-        _birdImage = downBirdImage;
-      }
+      // if (_birdY + _step <= 1) {
+      //   _birdY += _step;
+      // }
+      _birdY = 1;
+      _birdImage = downBirdImage;
+      // curve = Curves.easeInToLinear;
     });
   }
 }
