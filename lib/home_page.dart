c05c64a8f8b36679de3ceabe7eaebb838b48a17b
Yulu Xu <xuyulu86@126.com>
2022-10-18 23:18:29 +0800

    add bird jump up and down

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,14 @@
-import 'package:flappy_bird/flappy_bird_game.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double birdY = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +17,50 @@ class HomePage extends StatelessWidget {
         title: const Text('Flappy Bird'),
         centerTitle: true,
       ),
-      body: const FlappyBirdGame(),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            birdY -= 0.5;
+          });
+        },
+        child: Bird(
+          birdY: birdY,
+          onEnd: onJumpEnd,
+        ),
+      ),
+    );
+  }
+
+  void onJumpEnd() {
+    setState(() {
+      birdY = 1;
+    });
+  }
+}
+
+class Bird extends StatelessWidget {
+  const Bird({
+    Key? key,
+    required this.birdY,
+    required this.onEnd,
+  }) : super(key: key);
+
+  final double birdY;
+  final VoidCallback onEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      alignment: Alignment(-0.8, birdY),
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.linearToEaseOut,
+      onEnd: onEnd,
+      child: Container(
+        color: Colors.white,
+        width: 40,
+        height: 40,
+        child: Image.asset('assets/images/bird0_0.png'),
+      ),
     );
   }
 }
